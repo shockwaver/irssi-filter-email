04dd775485e07a9789dfdcb33d60a4b4502519c2
Chris Taylor <public_github@shockwaver.org>
2012-10-05 09:50:44 -0500

    first crack
    First try at the regexs to replace @ with [@]

--- filteremail.pl
+++ filteremail.pl
@@ -16,3 +16,14 @@ our %IRSSI = (
 			  .  'example@domain.com -> example[@]domain.com',
   license     => 'Public Domain',
 );
+
+Irssi::signal_add_first 'send text', 'filter_email';
+
+sub filter_email {
+    my ($text, $server, $win_item) = @_;
+	# check for email address
+	if ($text=~m/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;) {
+		$text=~s/@/\[@\]/g;
+	}
+	Irssi::signal_continue($text, $server, $win_item);
+}
